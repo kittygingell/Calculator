0d0b058f4c6ebbd0f6de1dd1588badb43d3e902b
Kitty Gingell <kittygingell@Kittys-MacBook.local>
2021-06-27 14:40:06 +0100

    Simple multiplying calculator

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const readline = require('readline-sync');
 console.log('Welcome to the calculator!');
+
+console.log('\nEnter your first number:');
+const N1 = readline.prompt();
+
+console.log('\nEnter your second number:');
+const N2 = readline.prompt();
+
+const answer = N1 * N2;
+console.log('\nThe answer is ' + answer);
